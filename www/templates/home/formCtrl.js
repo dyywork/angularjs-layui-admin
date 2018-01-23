31018ea4439c46c685c0625364091c1685a2f318
dyy <yahya_dyy@163.com>
2018-01-23 15:35:03 +0800

    表单页面

---
+++ www/templates/home/formCtrl.js
@@ -0,0 +1,15 @@
+define(function () {
+  'use strict';
+
+  var IosSelect = require('IosSelect');
+
+  return ['', function () {
+    var layer = layui.layer
+      , form = layui.form,
+      element = layui.element;
+
+    form.render()
+
+
+  }];
+});
